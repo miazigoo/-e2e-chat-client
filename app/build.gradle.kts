4e6db23793fd7ab667fd58c8618b02f2742d56c4
miazigoo <kasper6024@gmail.com>
2026-05-02 12:31:26 +0300

    chore: bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.example.securechatapp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
